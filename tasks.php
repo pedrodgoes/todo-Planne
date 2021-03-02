fc47a0558cff1c5c34639d3b7e83e060bf9fa219
Pedro Góes <pedrogoesbj@gmail.com>
2021-03-02 16:23:25 -0400

    commit final

--- tasks.php
+++ tasks.php
@@ -6,24 +6,29 @@ $sql = 'SELECT * FROM tasks';
 $res = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($res) > 0) {
+    if (mysqli_num_rows($res) == 1) {
+        echo '<div class="pending">Você tem ' .
+            mysqli_num_rows($res) .
+            ' nota pendente.</div>';
+    } else {
+        echo '<div class="pending">Você tem ' .
+            mysqli_num_rows($res) .
+            ' notas pendentes.</div>';
+    }
     while ($row = mysqli_fetch_assoc($res)) { ?>
-
 <li>
     <div class="note">
     <span class="text title"><?php echo $row['title']; ?></span>
-    <span class="text"><?php echo $row['description']; ?></span>
+    <span class="text description"><?php echo $row['description']; ?></span>
     </div>
     <i id="deleteBtn" class="icon fa fa-trash" 
     data-id="<?php echo $row['id']; ?>"></i>
     <input type="hidden">
 </li>
 
+
 <?php }
-    echo '<div class="pending">Você tem ' .
-        mysqli_num_rows($res) .
-        ' notas pendentes.</div>';
 } else {
     echo '<div class="pending">Você não possui notas pendentes.</div>';
 }
-
 ?>
